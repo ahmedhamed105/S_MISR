2bd04cf3fd29fd73c955ecd8efed2b2001c46d1c
Ahmed Elemam <ahmed.elemam@its.ws>
2018-07-14 23:05:42 +0200

    return to offline

--- APP/MISR/src/refund.c
+++ APP/MISR/src/refund.c
@@ -73,7 +73,6 @@ DWORD RefundTrans(DWORD aInputMode)
 	 IncTraceNo();
 	   PackHeader();
     RSP_DATA.b_response = TRANS_ACP;
-	 INPUT.b_trans_status = OFFLINE;
     IncTraceNoEND();
     GenAuthCode(RSP_DATA.sb_auth_code);
     PackDTGAA();
